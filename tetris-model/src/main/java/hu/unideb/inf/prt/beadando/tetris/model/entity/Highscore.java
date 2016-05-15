40c987d0f2e1dfcbd373321a5bc9a439bf9ffc4f
Fekete Attila <atkavagyok@gmail.com>
2016-05-15 15:27:51 +0200

    Highscore entity added

---
+++ tetris-model/src/main/java/hu/unideb/inf/prt/beadando/tetris/model/entity/Highscore.java
@@ -0,0 +1,51 @@
+package hu.unideb.inf.prt.beadando.tetris.model.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Highscore implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "player_name")
+	private String playerName;
+	
+	private Long score;
+
+	public Highscore() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public Long getScore() {
+		return score;
+	}
+
+	public void setScore(Long score) {
+		this.score = score;
+	}
+}
